1533116250b1ec487c083987c9c4ab4ba550948e
Evans Sitibekiso <jevacrism@gmail.com>
2022-03-29 11:06:01 +0200

    made changes on app.js

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-import Books from './modules/Books';
+import Books from "./modules/Books.js";
 
-const displayBook = document.querySelector('.display-books');
-const form = document.querySelector('form');
+const displayBook = document.querySelector(".display-books");
+const form = document.querySelector("form");
 
 // create an object
 const listBooks = new Books();
 const onPageReload = () => {
   displayBook.innerHTML = listBooks.books
-    .map((book) => (
-  <div class='display'>
-    <p>'${book.title}' by ${book.author}</p>
-    <button id='btn' class='remove-btn'>Remove</button>
-    </div>)).join('');
+    .map(
+      (book) =>
+        `<div class="display">
+        <p>
+          '${book.title}' by ${book.author}
+        </p>
+        <button id="btn" class="remove-btn">
+          Remove
+        </button>
+      </div>`
+    )
+    .join("");
 
   if (listBooks.books.length === 0) {
-    displayBook.style.cssText = 'border: none;';
+    displayBook.style.cssText = "border: none;";
   } else {
-    displayBook.style.cssText = 'border: 3px solid black;';
+    displayBook.style.cssText = "border: 3px solid black;";
   }
 };
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   const formData = new FormData(form);
-  const title = formData.get('title');
-  const author = formData.get('author');
+  const title = formData.get("title");
+  const author = formData.get("author");
   const bookObj = {
     title,
     author,
@@ -34,12 +41,18 @@ form.addEventListener('submit', (event) => {
   form.reset();
 });
 
-const btn = document.getElementById('btn');
-const removeBook = (bookId) => {
-  listBooks.removeBook(bookId);
-  onPageReload();
-};
+// const btn = document.querySelector(".display");
+// const removeBook = (bookId) => {
+//   listBooks.removeBook(bookId);
+//   onPageReload();
+// };
 
-btn.addEventListener('click', removeBook);
+displayBook.addEventListener("click", (e) => {
+  if (e.target.className === "remove-btn") {
+    listBooks.removeBook(e);
+    onPageReload();
+  }
+});
+
 onPageReload();
-document.querySelector('span').innerHTML = new Date();
+document.querySelector("span").innerHTML = new Date();
